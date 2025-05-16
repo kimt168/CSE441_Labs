54d8f89dfb36ea1c3052b36288361f0147c15664
kimt168 <luukimthi1502@gmail.com>
2025-05-16 14:27:47 +0700

    hihi

--- Lab04/app/src/main/java/com/example/lab04/MainActivity.java
+++ Lab04/app/src/main/java/com/example/lab04/MainActivity.java
@@ -10,15 +10,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtC,edtF;
+    Button btncf,btnfc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtC = findViewById(R.id.edtdoC);
+        edtF = findViewById(R.id.edtdoF);
+        btncf = findViewById(R.id.btncf);
+        btnfc = findViewById(R.id.btnfc);
+        btncf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                DecimalFormat dcf=new DecimalFormat("#.00");
+                String doC = edtdoC.getText()+"";
+                //
+                int C=Integer.parseInt(doC);
+                edtdoF.setText(""+dcf.format(C*1.8+32));
+
+            }
+        });
+        btnfc.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf=new DecimalFormat("#.00");
+                // TODO Auto-generated method stub
+                String doF = edtdoF.getText()+"";
+                int F=Integer.parseInt(doF);
+                edtdoC.setText(""+dcf.format((F-32)/1.8));
+            }
         });
     }
-}
