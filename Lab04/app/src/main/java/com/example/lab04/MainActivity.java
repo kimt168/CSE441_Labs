e0dd1e6f3e8b37b46cc3c1ca5478885e52a40695
kimt168 <luukimthi1502@gmail.com>
2025-05-16 14:31:22 +0700

    hihi

--- Lab04/app/src/main/java/com/example/lab04/MainActivity.java
+++ Lab04/app/src/main/java/com/example/lab04/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lab04;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,39 +11,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtC,edtF;
-    Button btncf,btnfc;
+    EditText edtC, edtF;
+    Button btnC, btnF;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edtC = findViewById(R.id.edtdoC);
-        edtF = findViewById(R.id.edtdoF);
-        btncf = findViewById(R.id.btncf);
-        btnfc = findViewById(R.id.btnfc);
-        btncf.setOnClickListener(new View.OnClickListener() {
+        edtC = findViewById(R.id.edtC);
+        edtF = findViewById(R.id.edtF);
+        btnC = findViewById(R.id.btnF);
+        btnF = findViewById(R.id.btnC);
+        btnC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                DecimalFormat dcf=new DecimalFormat("#.00");
-                String doC = edtdoC.getText()+"";
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String doC = edtC.getText() + "";
                 //
-                int C=Integer.parseInt(doC);
-                edtdoF.setText(""+dcf.format(C*1.8+32));
+                int C = Integer.parseInt(doC);
+                edtF.setText("" + dcf.format(C * 1.8 + 32));
 
             }
         });
-        btnfc.setOnClickListener(new View.OnClickListener() {
+        btnF.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                DecimalFormat dcf=new DecimalFormat("#.00");
+                DecimalFormat dcf = new DecimalFormat("#.00");
                 // TODO Auto-generated method stub
-                String doF = edtdoF.getText()+"";
-                int F=Integer.parseInt(doF);
-                edtdoC.setText(""+dcf.format((F-32)/1.8));
+                String doF = edtF.getText() + "";
+                int F = Integer.parseInt(doF);
+                edtC.setText("" + dcf.format((F - 32) / 1.8));
             }
         });
     }
+}
