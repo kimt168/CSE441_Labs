0a038b33c0d01e87123c676795ef70ceb2c482f4
kimt168 <luukimthi1502@gmail.com>
2025-05-29 08:02:49 +0700

    hihi

--- Lab0722/app/src/main/java/com/example/lab072/MainActivity.java
+++ Lab0722/app/src/main/java/com/example/lab072/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab072;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    EditText txta,txtb;
+    Button btnResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        txta = findViewById(R.id.txta);
+        txtb = findViewById(R.id.txtb);
+        btnResult = findViewById(R.id.btnResult);
+        btnResult.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent myintent = new Intent(MainActivity.this,ketqua.class);
+                Bundle bundle1 = new Bundle();
+                int a = Integer.parseInt(txta.getText()+"");
+                int b = Integer.parseInt(txtb.getText()+"");
+                bundle1.putInt("soa", a);
+                bundle1.putInt("sob",b);
+                myintent.putExtra("mybackage", bundle1);
+                startActivity(myintent);
+            }
         });
     }
 }
