17d94999af203cf083273e03bae50d78220e0393
kimt168 <luukimthi1502@gmail.com>
2025-06-03 15:15:11 +0700

    hihi

--- Lab12_2/app/src/main/java/com/example/lab12_2/MainActivity.java
+++ Lab12_2/app/src/main/java/com/example/lab12_2/MainActivity.java
@@ -1,93 +1,102 @@
 package com.example.lab12_2;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Khai báo các biến
     ListView lv;
     ArrayList<String> arraywork;
     ArrayAdapter<String> arrAdapater;
-    EditText edtwork,edth,edtm;
+    EditText edtwork, edth, edtm;
     TextView txtdate;
     Button btnwork;
+    SharedPreferences sharedPreferences;
+    final String PREF_NAME = "WORK_PREF";
+    final String KEY_WORK_LIST = "WORK_LIST";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         edth = findViewById(R.id.edthour);
         edtm = findViewById(R.id.edtmi);
         edtwork = findViewById(R.id.edtwork);
         btnwork = findViewById(R.id.btnadd);
         lv = findViewById(R.id.listView1);
         txtdate = findViewById(R.id.txtdate);
-////Ở đây chúng ta không sử dụng dữ liệu cố định, mà dữ liệu của //Listview được cập nhật
-//        trong quá trình chạy, nên ở đây ta
-//khai báo mảng ArrayList kiểu String rỗng
-                arraywork = new ArrayList<>();
-//Khai báo ArrayAdapter, đưa mảng dữ liệu vào ArrayAdapter
-        arrAdapater = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,arraywork);
-//Đưa Adapter có dữ liệu lên Listview
-        lv.setAdapter(arrAdapater);
-//Lấy ngày giờ hệ thống
-        Date currentDate = Calendar.getInstance().getTime();
-//Format theo định dạng dd/mm/yyyy
-        java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat("dd/MM/yyyy");
-//Hiển thị lên TextView
-        txtdate.setText("Hôm Nay: "+simpleDateFormat.format(currentDate));
-//Viết phương thức khi Click vào Button btnwork
-        btnwork.setOnClickListener(new View.OnClickListener() {
 
+        sharedPreferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+
+        // Load data from SharedPreferences
+        arraywork = new ArrayList<>();
+        String savedList = sharedPreferences.getString(KEY_WORK_LIST, "");
+        if (!savedList.isEmpty()) {
+            arraywork.addAll(Arrays.asList(savedList.split("\n")));
+        }
+
+        arrAdapater = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arraywork);
+        lv.setAdapter(arrAdapater);
+
+        Date currentDate = Calendar.getInstance().getTime();
+        java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat("dd/MM/yyyy");
+        txtdate.setText("Hôm Nay: " + simpleDateFormat.format(currentDate));
+
+        btnwork.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-////Nếu 1 trong 3 Edit Text không có nội dung thì hiện lên thông báo bằng hộp thoại
-//                Dialog
-
-                if(edtwork.getText().toString().equals("")||edth.getText().toString().equals("") ||edtm.getText().toString().equals("")) {
+                if (edtwork.getText().toString().equals("") || edth.getText().toString().equals("") || edtm.getText().toString().equals("")) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                     builder.setTitle("Info missing");
                     builder.setMessage("Please enter all information of the work");
-                    builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            // TODO Auto-generated method stub
-                        }
-                    });
+                    builder.setPositiveButton("Continue", null);
                     builder.show();
-                }
-//                //Lấy nội dung công việc và thời gian ra từ Edit Text và đưa vào Mảng arraywork, cập
-//                nhật lại Adapter
-    else {
-                    String str = edtwork.getText().toString() + " - "+
-                            edth.getText().toString() + ":"+edtm.getText().toString();
-//Để Thêm dữ liệu cho Listview, chúng ta cần 2 bước:
-                    //Cập Thêm liệu cho mảng
-                    arraywork.add(str);  //Xóa: arraywork.remove(i)
-                    //Cập nhật lại Adapter
+                } else {
+                    String str = edtwork.getText().toString() + " - " +
+                            edth.getText().toString() + ":" + edtm.getText().toString();
+                    arraywork.add(str);
                     arrAdapater.notifyDataSetChanged();
+                    saveListToPreferences();
                     edth.setText("");
                     edtm.setText("");
                     edtwork.setText("");
                 }
             }
         });
+
+        // Delete item on click
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                arraywork.remove(position);
+                arrAdapater.notifyDataSetChanged();
+                saveListToPreferences();
+            }
+        });
+    }
+
+    // Save the list to SharedPreferences
+    private void saveListToPreferences() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : arraywork) {
+            sb.append(s).append("\n");
+        }
+        sharedPreferences.edit().putString(KEY_WORK_LIST, sb.toString().trim()).apply();
     }
 }
