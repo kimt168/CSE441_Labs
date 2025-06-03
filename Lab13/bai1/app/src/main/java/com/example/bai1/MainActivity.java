10c11d07f030d2b30900d4d42116f7f52141346e
kimt168 <luukimthi1502@gmail.com>
2025-06-03 22:28:39 +0700

    hiihi

--- Lab13/bai1/app/src/main/java/com/example/bai1/MainActivity.java
+++ Lab13/bai1/app/src/main/java/com/example/bai1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.bai1;
 
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * This is the main activity of the application.
+     * It sets up the layout and handles window insets for edge-to-edge display.
+     */
+    TextView selection;
+    AutoCompleteTextView singleComplete;
+    MultiAutoCompleteTextView multiComplete;
+    String array[] = {
+            "hà nội", "huế", "Sài gòn", "Hà giang", "Kien giang",
+            "Lâm Đồng", "Long khánh"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        singleComplete = (AutoCompleteTextView) findViewById(R.id.selection);
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
