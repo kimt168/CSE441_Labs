d0494019eb165b2d25903a3d157476388168b399
kimt168 <luukimthi1502@gmail.com>
2025-06-10 09:54:07 +0700

    bai1

--- Lab13.2/bai1/app/src/main/java/com/example/bai1/phone.java
+++ Lab13.2/bai1/app/src/main/java/com/example/bai1/phone.java
@@ -1,4 +1,28 @@
 package com.example.bai1;
 
 public class phone {
+    private String namephone;
+    private int imagephone;
+
+    public phone(String namephone, int imagephone) {
+        this.namephone = namephone;
+        this.imagephone = imagephone;
+    }
+
+    public String getNamephone() {
+        return namephone;
+    }
+
+    public void setNamephone(String namephone) {
+        this.namephone = namephone;
+    }
+
+    public int getImagephone() {
+        return imagephone;
+    }
+
+    public void setImagephone(int imagephone) {
+        this.imagephone = imagephone;
+    }
 }
+    }
