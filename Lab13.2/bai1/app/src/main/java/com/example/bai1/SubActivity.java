fe77ea625cfd8573d7a4330399131c2dbb4de087
kimt168 <luukimthi1502@gmail.com>
2025-06-10 10:46:19 +0700

    bai1

--- Lab13.2/bai1/app/src/main/java/com/example/bai1/SubActivity.java
+++ Lab13.2/bai1/app/src/main/java/com/example/bai1/SubActivity.java
@@ -1,4 +1,20 @@
 package com.example.bai1;
 
-public class SubActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+    TextView txt_subphone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        txt_subphone = findViewById(R.id.txt_subphone);
+        Intent myintent = getIntent();
+        String namephone = myintent.getStringExtra("name");
+        txt_subphone.setText(namephone);
+    }
 }
