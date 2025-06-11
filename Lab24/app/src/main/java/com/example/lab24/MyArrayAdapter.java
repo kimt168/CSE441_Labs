85311d8cff22b2cf39735c2cbd5c5d75869179d8
kimt168 <luukimthi1502@gmail.com>
2025-06-11 14:47:08 +0700

    24

---
+++ Lab24/app/src/main/java/com/example/lab24/MyArrayAdapter.java
@@ -0,0 +1,45 @@
+package com.example.lab24;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyArrayAdapter extends ArrayAdapter<Tygia> {
+    Activity context;
+    int resource;
+    List<Tygia> objects;
+
+    public MyArrayAdapter(Activity context, int resource, List<Tygia> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.objects = objects;
+        this.resource = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = this.context.getLayoutInflater();
+        View item = inflater.inflate(this.resource, null);
+        Tygia tygia = this.objects.get(position);
+        ImageView imgHinh = (ImageView) item.findViewById(R.id.imgHinh);
+        TextView txtType = (TextView) item.findViewById(R.id.txtType);
+        TextView txtBuyTM = (TextView) item.findViewById(R.id.txtBuyTM);
+        TextView txtSellTM = (TextView) item.findViewById(R.id.txtSellTM);
+        TextView txtBuyCK = (TextView) item.findViewById(R.id.txtBuyCK);
+        TextView txtSellCK = (TextView) item.findViewById(R.id.txtSellCK);
+        imgHinh.setImageBitmap(tygia.getBitmap());
+        txtType.setText(tygia.getType());
+        txtBuyTM.setText(tygia.getMuatienmat());
+        txtSellTM.setText(tygia.getBantuenmat());
+        txtBuyCK.setText(tygia.getMuack());
+        txtSellCK.setText(tygia.getBanck());
+
+        return item;
+    }
+}
