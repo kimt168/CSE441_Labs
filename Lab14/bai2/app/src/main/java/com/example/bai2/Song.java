2838a3f897b062043472ab5c12002840b89dbd87
kimt168 <luukimthi1502@gmail.com>
2025-06-11 09:46:35 +0700

    2

---
+++ Lab14/bai2/app/src/main/java/com/example/bai2/Song.java
@@ -0,0 +1,36 @@
+package com.example.bai2;
+public class Song {
+   private String title;
+    private boolean isFavorite;
+    private String id;
+
+    public Song( String id,String title, boolean isFavorite) {
+        this.title = title;
+        this.id = id;
+        this.isFavorite = isFavorite;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
